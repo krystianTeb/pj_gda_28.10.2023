56610b5884905a4c45d0c19d6858d81ee3e35d07
Krystian <krystian.lewandowski@teb.edu.pl>
2023-10-28 12:20:03 +0200

    18_petle_for_nieskonczona

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; ; i++){
                 System.out.println("Wartość i: " + i);
         }
 
