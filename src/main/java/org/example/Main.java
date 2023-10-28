b72f895a34a782e004fcbb9867792865f1749715
Krystian <krystian.lewandowski@teb.edu.pl>
2023-10-28 12:56:18 +0200

    30_tablice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        int tablica[] = {1, 2, 3, 4, 5};
+        int tablica[] = {1, 2, 3, 4, 5, 6, 7, 8};
         System.out.println("ilość elementów tanlicy: " + tablica.length );
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < tablica.length; i++){
             System.out.println(tablica[i]);
         }
 
