6dc55c783aec0b8911d756868974c0f1faf4f8b0
Krystian <krystian.lewandowski@teb.edu.pl>
2023-10-28 15:43:36 +0200

    42_przyklad_continue

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,19 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-        for(int i = 0; ;i++){
-            if(i > 10){
-                break;//kończy działanie pętli
+        //1 sposób z użyciem continue
+        for(int i = 0; i <= 10 ;i++){
+            if(i % 2 == 0){
+                continue;//przerwanie obecnej iteracji (obiegu pętli)
             }
             System.out.println(i);
+        }
+        //2 sposób bez użycia continue
+        for(int i = 0; i <= 10 ;i++){
+            if(i % 2 != 0){
+                System.out.println(i);
+            }
+
         }
 
 
