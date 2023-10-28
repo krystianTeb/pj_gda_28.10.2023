839e105d00a069263ce78d25ee6f6578e9f312fa
Krystian <krystian.lewandowski@teb.edu.pl>
2023-10-28 15:39:39 +0200

    41_przyklad_break

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,22 +3,13 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        //wersja 1
-        int i = 1;
-        do{
-            System.out.println(i % 2 == 0 ? i + " jest parzsta!": i + " jest NIEparzysta!");
-            i++;
-        }while(i <= 20);
-        //wersja 2
-        i = 1;
-        do{
-            if(i % 2 == 0) {
-                System.out.println(i + " jest parzsta!");
-            }else{
-                System.out.println(i + " jest NIEparzysta!");
+        for(int i = 0; ;i++){
+            if(i > 10){
+                break;//kończy działanie pętli
             }
-            i++;
-        }while(i <= 20);
+            System.out.println(i);
+        }
+
 
     }
 }
