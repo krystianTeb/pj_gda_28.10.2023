d50d6cf1f7ccce57c65f52f5d23ee2bce5fc5334
Krystian <krystian.lewandowski@teb.edu.pl>
2023-10-28 14:54:11 +0200

    36_cw1_149slajd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,15 +2,19 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-        int tablica[] = {1, 2, 3, 4, 5, 6, 7, 8};
-        System.out.println("ilość elementów tanlicy: " + tablica.length );
-        for(int value: tablica){ //foreach
-            if(value >= 2) {
-                System.out.println(value);
+        // 1 wersja
+        for(int i = 1; i <= 10; i++){
+            System.out.print(i % 2 == 0 ? i + "\n":"");
+        }
+        //2wersja
+        for(int i = 1; i <= 10; i++){
+            if(i % 2 == 0 ) System.out.println(i);
+        }
+        //3wersja
+        for(int i = 1; i <= 10; i++){
+            if(i % 2 == 0 ) {
+                System.out.println(i);
             }
         }
-
-
     }
 }
