4564b7ce96a26cd27f7f620bff1e309cb6c28578
Krystian <krystian.lewandowski@teb.edu.pl>
2023-10-28 11:51:40 +0200

    6_petle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         //Wypisanie wszystkich wartości podzielnych przez 3 z przedziału: 1 do 15
-        for(int i = 1; i <= 10; i++) {
+        for(int i = 1; i <= 15; i++) {
             if(i % 3 == 0) {
                 System.out.println("Liczba: " + i + "\tpodzielna przez 3");
             }
