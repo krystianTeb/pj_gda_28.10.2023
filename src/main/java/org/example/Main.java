cbf01c9939c9b5d430e58673ccea1368edd53ba9
Krystian <krystian.lewandowski@teb.edu.pl>
2023-10-28 13:31:56 +0200

    34_tablice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,10 @@ public class Main {
 
         int tablica[] = {1, 2, 3, 4, 5, 6, 7, 8};
         System.out.println("ilość elementów tanlicy: " + tablica.length );
-        for(int value: tablica){
-            System.out.println(value);
+        for(int value: tablica){ //foreach
+            if(value > 5) {
+                System.out.println(value);
+            }
         }
 
 
