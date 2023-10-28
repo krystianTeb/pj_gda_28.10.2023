89386e81744871c139bd68d4cba6dc4181bc3831
Krystian <krystian.lewandowski@teb.edu.pl>
2023-10-28 15:05:02 +0200

    38_cw3_for_149slajd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,26 +4,16 @@ public class Main {
     public static void main(String[] args) {
 
         //wersja 1
-        int i = 1;
-        while(i <= 20){
-            System.out.print(i % 3 != 0 ? i + "\n": "");
-            i++;
+        for(int i = 1; i <= 20; i++){
+            System.out.println(i % 2 == 0 ? i + " jest parzsta!": i + " jest NIEparzysta!");
         }
-
         //wersja 2
-        int j = 1;
-        while(j <= 20){
-            if (j % 3 != 0) System.out.println( j );
-            j++;
-        }
-
-        //wersja 3
-        int x = 1;
-        while(x <= 20){
-            if (x % 3 != 0) {
-                System.out.println( x );
+        for(int i = 1; i <= 20; i++){
+            if(i % 2 == 0) {
+                System.out.println(i + " jest parzsta!");
+            }else{
+                System.out.println(i + " jest NIEparzysta!");
             }
-            x++;
         }
 
     }
