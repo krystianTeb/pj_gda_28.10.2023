cca526bdc696ea7304061163ed5a80d6d2630aa9
Krystian <krystian.lewandowski@teb.edu.pl>
2023-10-28 11:55:17 +0200

    8_petle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         //Wypisanie wszystkich wartości podzielnych przez 3 z przedziału: 0 do 60 zwiększane o 10
         for(int i = 0; i <= 60; i+=10) {
-            if(i % 3 == 0) {
+            if(i % 3 == 0 && i != 0) {
                 System.out.println("Liczba: " + i + "\tpodzielna przez 3");
             }
         }
