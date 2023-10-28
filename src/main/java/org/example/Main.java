9d3d89fd1a80b2b33a1d38ec8bb351257053ece3
Krystian <krystian.lewandowski@teb.edu.pl>
2023-10-28 12:40:43 +0200

    24_petle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        int i = 0;                   // wyrażenie początkowe
+        int i = 0;//wyrażenie inicjujące
         do{
-            System.out.println(i++); //wypisanie wartości i i (inkrementacja) wyrażenie modyfikujące
-        }while(false);              //sprawdzenie warunku
-
+            /*to co ma się wykonać w obiegu pętli*/
+            System.out.println("[i = " + i + "] obieg pętli");
+        }
+        while(++i < 4);// wyrażenie modyfikujące i wyrażenie warunkowe
 
     }
 }
