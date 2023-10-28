5e3c7c40973d2510b1f9bf9516412abb42ab65fe
Krystian <krystian.lewandowski@teb.edu.pl>
2023-10-28 15:09:56 +0200

    40_cw3_do_while_149slajd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,20 +5,20 @@ public class Main {
 
         //wersja 1
         int i = 1;
-        while(i <= 20){
+        do{
             System.out.println(i % 2 == 0 ? i + " jest parzsta!": i + " jest NIEparzysta!");
             i++;
-        }
+        }while(i <= 20);
         //wersja 2
         i = 1;
-        while(i <= 20){
+        do{
             if(i % 2 == 0) {
                 System.out.println(i + " jest parzsta!");
             }else{
                 System.out.println(i + " jest NIEparzysta!");
             }
             i++;
-        }
+        }while(i <= 20);
 
     }
 }
