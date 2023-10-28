b752a0272a7c069ce6a55bd7f1fcf4e81890531e
Krystian <krystian.lewandowski@teb.edu.pl>
2023-10-28 12:52:02 +0200

    27_tablice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        int i = 0;//wyrażenie inicjujące
-        do{
-            /*to co ma się wykonać w obiegu pętli*/
-            System.out.println("[i = " + i++ + "] obieg pętli");
-        }
-        while(i < 4);// wyrażenie modyfikujące i wyrażenie warunkowe
-
+        int i[] = {1, 2, 3, 4, 5};
+        System.out.println(i[0]);
+        System.out.println(i[1]);
+        System.out.println(i[2]);
+        System.out.println(i[3]);
+        System.out.println(i[4]);
+        System.out.println("ilość elementów tanlicy: " + i.length );
     }
 }
