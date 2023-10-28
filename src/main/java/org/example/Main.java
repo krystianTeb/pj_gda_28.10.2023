83143278365269bb9ee4205fd60fbfb6804bf5d2
Krystian <krystian.lewandowski@teb.edu.pl>
2023-10-28 11:41:25 +0200

    5_petle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,12 @@
 package org.example;
-import java.util.Locale;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-
-        int licznik = 0;
-        while(licznik < 2) {
-            licznik++; // inkrementacja (zwiękczenie licznik o + 1)
-            System.out.println("Wprowadź liczbę do weryfikacji: [potwierdź enterem]");
-            int liczba = scan.nextInt();
-
-            System.out.println(liczba % 2 == 0 ? "Liczba jest parzysta!" : "Liczba nieparzysta!");
+        for(int i = 1; i <= 10; i++) { // i to iterator (licznik)
+            System.out.println("Zawodnik " + i);
         }
+
 
 
     }
