1b008f4833a303e162ac2bb88810f5d0aee46245
Krystian <krystian.lewandowski@teb.edu.pl>
2023-10-28 12:57:39 +0200

    31_tablice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         int tablica[] = {1, 2, 3, 4, 5, 6, 7, 8};
         System.out.println("ilość elementów tanlicy: " + tablica.length );
-        for(int i = 0; i < tablica.length; i++){
+        for(int i = 3; i < tablica.length; i++){
             System.out.println(tablica[i]);
         }
 
