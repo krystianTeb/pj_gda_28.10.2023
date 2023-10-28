8d8546fced692f849c64aec1b68ecb1f3866d34e
Krystian <krystian.lewandowski@teb.edu.pl>
2023-10-28 12:06:31 +0200

    12_petle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         for(int i = 0; i < 5;){
-            i++;
-            System.out.println("Wartość i: " + i);
 
+            System.out.println("Wartość i: " + i);
+            i++;
         }
 
 
